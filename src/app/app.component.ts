3520c76c24f296d969f5d980cd917810d02d510f
Ignacio <ignaciogazzia72@gmail.com>
2021-07-25 19:17:25 -0300

    Proyecto Incial: Curso Angular 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] //Es el lenguaje base para los estilos. Aca se indica la URL de nuestro fichero de estilos .css
 })
 export class AppComponent {
-  title = 'listaEjercicios';
+  //Solo hay una propiedad que se llama title
+  title = 'Lista de Ejercicios';
 }
